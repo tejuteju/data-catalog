5323226ee738c3ab9ea6833adab695d417d725d9
Tejaswi <34575326+tejuteju@users.noreply.github.com>
2022-08-03 13:11:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "google_project_iam_member" "dc_entrygroup_owner" {
 resource "google_data_catalog_entry" "basic_entry" {
   entry_group = google_data_catalog_entry_group.entry_group.id
   entry_id = var.entry_id
-
+  //linked_resource = "dc"
   type = "FILESET"
 
   gcs_fileset_spec {
@@ -29,4 +29,51 @@ resource "google_data_catalog_entry" "basic_entry" {
 resource "google_data_catalog_entry_group" "entry_group" {
   entry_group_id = var.entry_group_id
   
+}
+resource "google_data_catalog_tag_template" "example" {  
+  tag_template_id = var.tag_template_id
+  region          = var.region 
+  display_name    = "Required Tags"  
+  fields {    
+    field_id = "data_classification"    
+    display_name = "Data Classification"    
+    type {      
+      enum_type {        
+        allowed_values {          
+          display_name = "PRIVATE"        
+        }        
+        allowed_values {          
+          display_name = "RESTRICTED"        
+        }        
+        allowed_values {          
+          display_name = "PUBLIC"        
+        }      
+      }
+    }
+    is_required = true
+  }
+  force_delete = true
+}
+resource "google_data_catalog_taxonomy" "basic_taxonomy" {
+  project = var.project_id
+  provider = google-beta
+  region = "us"
+  display_name =  "my_display_name"
+  description = "A collection of policy tags"
+  activated_policy_types = ["FINE_GRAINED_ACCESS_CONTROL"]
+}
+resource "google_data_catalog_policy_tag" "basic_policy_tag" {   
+  provider = google-beta
+  taxonomy = google_data_catalog_taxonomy.basic_taxonomy.id
+  display_name = "sample policy tag" 
+  description = "basic policy tag"
+}
+resource "google_data_catalog_tag" "basic_tag" {
+  parent   = google_data_catalog_entry.basic_entry.id
+  template = google_data_catalog_tag_template.example.id
+
+  fields {
+    field_name   = "data_classification"
+    enum_value = "PRIVATE"
+  }
 }
